9a19716ccbaf3c9c6f85bd8b2818ded94329766a
Jesse Millar <hellojessemillar@gmail.com>
2016-06-03 15:29:59 -0600

    Adding a help page

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,37 @@ function wakeSystem() {
 function displayVersion() {
     $(".version-number").text("version " + version);
 }
+
+function confirmPowerOff() {
+    swal({
+        title: "Turn system off?",
+        text: "I'll miss you!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes",
+        cancelButtonText: "No",
+        closeOnConfirm: false,
+        closeOnCancel: false
+    }, function(isConfirm) {
+        if (isConfirm) {
+            swal({
+                title: "Powering off!",
+                text: "The system will now shut down.",
+                type: "success",
+                timer: 2000,
+                showConfirmButton: false
+            });
+        } else {
+            swal.close();
+        }
+    });
+}
+
+function showControlPage() {
+    bootpage.switch("control-page");
+}
+
+function showHelpPage() {
+    bootpage.switch("help-page");
+}
