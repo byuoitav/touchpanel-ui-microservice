8bfcdba50fa0d38332f19a6f867ce5f61226a7d1
Jesse Millar <hellojessemillar@gmail.com>
2016-07-29 16:01:21 -0600

    Fixing the link REF

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -6,9 +6,9 @@ function sonyTVPower() {
 
     $.ajax({
         type: "POST",
-        url: "localhost:8007/command",
+        url: "http://localhost:8007/command",
         data: homeButton,
-        success: swal("Command sent."),
+        success: sweetAlert("Yay!", "Command sent successfully!", "success"),
         contentType: "application/json; charset=utf-8"
     });
 }
