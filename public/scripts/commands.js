36a80bed80e23c0659ee37c9c7042e07875c5840
Jesse Millar <hellojessemillar@gmail.com>
2016-09-16 13:19:38 -0600

    Making some buttons function

--- public/scripts/commands.js
+++ public/scripts/commands.js
@@ -1,3 +1,33 @@
+function switchInput(input) {
+    console.log("Pressed");
+
+    var body = {
+        currentVideoInput: input,
+        displays: [{
+            name: "D2",
+            power: "on",
+            blanked: false
+        }]
+    };
+
+    console.log(body);
+
+    $.ajax({
+        type: "PUT",
+        url: "http://localhost:8000/buildings/ITB/rooms/1001D",
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        data: JSON.stringify(body),
+        success: sweetAlert("Yay!", "Command sent successfully!", "success"),
+        contentType: "application/json; charset=utf-8"
+    });
+}
+
+// ------------------------------------------------------
+// OLD API COMMANDS BELOW
+// ------------------------------------------------------
+
 function sonyTVPowerOn() {
     $.ajax({
         type: "PUT",
@@ -34,19 +64,21 @@ function sonyTVPowerOff() {
     });
 }
 
-function switchInput(inputName) {
-    var inputBody = {
-        "currentVideoInput": inputName
-    };
-
-    $.ajax({
-        type: "PUT",
-        url: "http://localhost:8000/buildings/ITB/rooms/1110",
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        data: JSON.stringify(inputBody),
-        success: sweetAlert("Yay!", "Command sent successfully!", "success"),
-        contentType: "application/json; charset=utf-8"
-    });
-}
+// function switchInput(inputName) {
+//     var inputBody = {
+//         "currentVideoInput": inputName
+//     };
+//
+//     $.ajax({
+//         type: "PUT",
+//         url: "http://localhost:8000/buildings/ITB/rooms/1110",
+//         headers: {
+//             'Access-Control-Allow-Origin': '*'
+//         },
+//         data: JSON.stringify(inputBody),
+//         success: sweetAlert("Yay!", "Command sent successfully!", "success"),
+//         contentType: "application/json; charset=utf-8"
+//     });
+// }
+// /     });
+// }
