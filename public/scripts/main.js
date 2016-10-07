e13b1eabbee85949f2d1d1ecb87c61fe74cde6e0
Jesse Millar <hellojessemillar@gmail.com>
2016-10-07 11:41:09 -0600

    Adding a logging message

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -41,6 +41,8 @@ function getVolume() {
             'Access-Control-Allow-Origin': '*'
         },
         success: function(data) {
+            console.log("Returned volume data: " + data);
+
             var devices = data.devices;
             for (var i = 0; i < devices.length; i++) {
                 if (devices[i].name == "D1") {
