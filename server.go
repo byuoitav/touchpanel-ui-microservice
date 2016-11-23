003d86aea2b3db62d5f37a1d01365cde4e737a17
Jesse Millar <hellojessemillar@gmail.com>
2016-11-23 11:03:43 -0700

    Vendoring dependencies and moving to Echo v3

--- server.go
+++ server.go
@@ -2,34 +2,32 @@ package main
 
 import (
 	"html/template"
-	"log"
+	"net/http"
 
 	"github.com/byuoitav/raspi-tp/helpers"
 	"github.com/byuoitav/raspi-tp/views"
 	"github.com/jessemillar/health"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/fasthttp"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	port := ":8888"
+	router := echo.New()
+	router.Pre(middleware.RemoveTrailingSlash())
+	router.Use(middleware.CORS())
+
 	templateEngine := &helpers.Template{
 		Templates: template.Must(template.ParseGlob("public/*/*.html")),
 	}
 
-	port := ":8888"
-	router := echo.New()
-	router.Pre(middleware.RemoveTrailingSlash())
-	router.SetRenderer(templateEngine)
+	router.Renderer = templateEngine
 
-	router.Use(middleware.CORS())
-
-	router.Get("/health", health.Check)
+	router.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
 
 	// Views
 	router.Static("/*", "public")
-	router.Get("/", views.Main)
+	router.GET("/", views.Main)
 
-	log.Println("Raspi TP is listening on " + port)
-	router.Run(fasthttp.New(port))
+	router.Start(port)
 }
