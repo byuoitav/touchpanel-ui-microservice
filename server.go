b1bb0a5c79634891638725df3b6149e8d7171838
Daniel Randall <dgrandall@me.com>
2017-02-21 23:30:47 +0000

    enable mouse, get room (in go)

--- server.go
+++ server.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strings"
+	"time"
 
+	"github.com/byuoitav/av-api/dbo"
 	"github.com/byuoitav/raspi-tp/helpers"
 	"github.com/byuoitav/raspi-tp/views"
 	"github.com/jessemillar/health"
@@ -24,6 +28,49 @@ func main() {
 	router.Renderer = templateEngine
 
 	router.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
+
+	// get hostname, get room info based on hostname UNCOMMENT FOR USAGE
+	// hostname, err := os.Hostname()
+	// if err != nil {
+	// 	log.Printf("couldn't get the hostname. error: %s", err)
+	// }
+
+	// temporary
+	hostname := "ITB-1006"
+
+	splitValues := strings.Split(hostname, "-")
+	log.Printf("Room: %v-%v", splitValues[0], splitValues[1])
+
+	attempts := 0
+	room, err := dbo.GetRoomByInfo(splitValues[0], splitValues[1])
+	// if there is an error retrieving the room info, try again a few times (configuration service may not be up)
+	if err != nil {
+		//If there was an error we want to attempt to connect multiple times - as the
+		//configuration service may not be up.
+		for attempts < 40 {
+			log.Printf("Attempting to connect to DB...")
+			room, err = dbo.GetRoomByInfo(splitValues[0], splitValues[1])
+			if err != nil {
+				log.Printf("error: %s", err)
+				attempts++
+				time.Sleep(2 * time.Second)
+			} else {
+				break
+			}
+		}
+		if attempts > 30 && err != nil {
+			log.Printf("Error Retrieving room information.")
+		}
+	}
+
+	devices, err := dbo.GetDevicesByRoom(room.Building.Name, room.Name)
+	if err == nil {
+		log.Print("no devices available in room")
+	} else {
+		if devices == nil {
+
+		}
+	}
 
 	// Views
 	router.Static("/*", "public")
