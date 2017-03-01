0ab4936ac24e3af2680a6826fbccab17f59509cf
Jesse Millar <hellojessemillar@gmail.com>
2017-03-01 13:20:03 -0700

    I'm a dummy

--- views/views.go
+++ views/views.go
@@ -11,16 +11,14 @@ import (
 func Main(context echo.Context) error {
 	log.Println("Returning page")
 
-	if len(os.Getenv("PI_HOSTNAME")) > 0 { // get development hostname
-		hostname, err := os.Getenv("PI_HOSTNAME")
-		if err != nil {
-			log.Printf("Couldn't get development hostname: %s", err)
-		}
-	} else { // get the real hostname
-		hostname, err := os.Hostname()
-		if err != nil {
-			log.Printf("Couldn't get hostname: %s", err)
-		}
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Printf("Couldn't get hostname: %s", err)
+	}
+
+	// get the development hostname instead of the real one if it's set
+	if len(os.Getenv("PI_HOSTNAME")) > 0 {
+		hostname = os.Getenv("PI_HOSTNAME")
 	}
 
 	return context.Render(http.StatusOK, "main", hostname)
