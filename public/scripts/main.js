7826d6ae685b098f4e6c5dd042cb4737b8e22bb2
Daniel Randall <dgrandall@me.com>
2017-03-27 13:08:22 -0600

    finished

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -22,11 +22,13 @@ function getRoom() {
 	console.log("hostname =", hostname);
 	var split = hostname.split('-');
 
-	url = "http://" + hostname +":8000/buildings/" + split[0] + "/rooms/" + split[1];
-	rpcUrl = "http://" + hostname + ":8100/buildings/" + split[0] + "/rooms/" + split[1];
+	var urlHost = document.location.host;
+	urlHost = /.+?(?=:)/.exec(urlHost) // remove port info
+	console.log("urlHost =", urlHost);
+	url = "http://" + urlHost  + ":8000/buildings/" + split[0] + "/rooms/" + split[1];
+	rpcUrl = "http://" + urlHost + ":8100/buildings/" + split[0] + "/rooms/" + split[1];
 	console.log("url for this room: ", url);
 
-	console.log("here");
 	getAllData();
 
 	// get devices, put them into an array
