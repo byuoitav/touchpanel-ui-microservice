0ab34cf9015b5870e3dec83ebae38bd93b71fe4e
Daniel Randall <dgrandall@me.com>
2017-05-11 15:41:17 -0600

    added socket service

---
+++ touchpanel-ui/src/app/socket.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, EventEmitter } from '@angular/core'
+
+@Injectable()
+export class SocketService {
+	private socket: WebSocket;
+	private listener: EventEmitter<any> = new EventEmitter();
+
+	public constructor() {
+		this.socket = new WebSocket("ws://localhost:");
+
+		this.socket.onopen = event => {
+			this.listener.emit({"type": "open", "data": event});
+		}
+
+		this.socket.onclose = event => {
+			this.listener.emit({"type": "close", "data", event});
+		}
+
+		this.socket.onmessage = event => {
+			console.log("Message recieved");
+			console.log(event);
+			this.listener.emit({"type": "messsage", "data": event});
+		}
+	}
+
+	public close() {
+		this.socket.close();
+	}
+
+	public getEventListener() {
+		return this.listener;
+	}
+}
