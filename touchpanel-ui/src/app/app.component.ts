86d7ff656999a6ffa278c56e14d07e89c08d9a3d
Daniel Randall <dgrandall@me.com>
2017-06-16 14:10:00 -0600

    fill screen on different size

--- touchpanel-ui/src/app/app.component.ts
+++ touchpanel-ui/src/app/app.component.ts
@@ -79,7 +79,7 @@ export class AppComponent { // event stuff
     this.getData();
 	this.blanked = true;
 	this.helprequested = false;
-    this.showing = true;
+//    this.showing = true;
 
     // setup socket to recieve events
     this.socket.getEventListener().subscribe(event => {
