d84870e1cf3c6b281d6e3bc232ad7f218df0863b
Daniel Randall <dgrandall@me.com>
2017-06-21 10:10:08 -0600

    this should be everythihng?

--- deploy-pi.sh
+++ deploy-pi.sh
@@ -2,10 +2,12 @@
 
 if [ $1 = "development" ]; then
 	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS"_development
+elif [ $1 = "testing" ]; then
+	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS"_testing
 elif [ $1 = "stage" ]; then
 	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS"_stage
 elif [ $1 = "production" ]; then
 	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS"_production
 else
-	echo "Either 'development', 'stage', or 'production' must be passed as the sole argument"
+	echo "Either 'development', 'testing', 'stage', or 'production' must be passed as the sole argument"
 fi
