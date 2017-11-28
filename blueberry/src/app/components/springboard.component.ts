e2cc4068f18a91225506032103a3200ab872bdfb
dannyrandall <dgrandall@me.com>
2017-11-28 15:42:29 -0700

    breaks up inputs into pages. inputs now implement item interface.

--- blueberry/src/app/components/springboard.component.ts
+++ blueberry/src/app/components/springboard.component.ts
@@ -2,20 +2,27 @@ import { Component, Input, OnInit } from '@angular/core';
 
 
 export interface SpringboardItem {
-    getColor(): string;
-    getIcon(): string;
     getName(): string;
+    getDisplayName(): string;
+    getIcon(): string;
+    getColor(): string;
 }
 
-export class Page {
-    // an array with a max...
-    items: SpringboardItem[];
+class Page {
+    items: SpringboardItem[] = [];
 }
+
+const MAX_ITEMS = 3;
 
 @Component({
     selector: 'springboard',
     template: `
         <div class="basediv" [ngStyle]="{'height': size, 'width': size}">
+            <div *ngFor="let page of pages">
+                <div *ngFor="let item of page.items">
+                    <span>{{item.getName()}}</span>
+                </div>
+            </div>
         </div>
     `,
     styles: [`
@@ -39,13 +46,27 @@ export class SpringboardComponent implements OnInit {
     @Input() items: SpringboardItem[];
 
     // pages is a map of the page to its children
+    private pages: Page[] = [];
 
     constructor() {}
 
     ngOnInit() {
-    
+        setTimeout(() => {
+            this.breakup();
+        }, 0);
     }
 
     private breakup() {
+        let pageNum = -1;
+
+        for (let i = 0; i < this.items.length; ++i) {
+            if (i % MAX_ITEMS == 0) {
+                pageNum++; 
+                this.pages.push(new Page());
+            }
+            this.pages[pageNum].items.push(this.items[i]);
+        }
+
+        console.log("pages", this.pages);
     }
 }
