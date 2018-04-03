740b06b0611fb62bef9e64a1d755145f00117362
Daniel Randall <dgrandall@me.com>
2018-04-03 10:27:50 -0600

    remove cas

--- server.go
+++ server.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/byuoitav/authmiddleware"
 	"github.com/byuoitav/device-monitoring-microservice/statusinfrastructure"
 	"github.com/byuoitav/event-router-microservice/eventinfrastructure"
 	"github.com/byuoitav/touchpanel-ui-microservice/events"
@@ -31,7 +30,7 @@ func main() {
 	router := echo.New()
 	router.Pre(middleware.RemoveTrailingSlash())
 	router.Use(middleware.CORS())
-	router.Use(echo.WrapMiddleware(authmiddleware.AuthenticateUser))
+	// router.Use(echo.WrapMiddleware(authmiddleware.AuthenticateUser))
 
 	router.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
 	router.GET("/mstatus", GetStatus)
