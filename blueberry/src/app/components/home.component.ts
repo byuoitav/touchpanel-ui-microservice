f3ac2310c845cd57ebdc714a55e32fee4e3dc69d
jpw547 <jwerner547@gmail.com>
2018-09-17 14:26:31 -0600

    changing help message for after hours

--- blueberry/src/app/components/home.component.ts
+++ blueberry/src/app/components/home.component.ts
@@ -985,4 +985,46 @@ export class HomeComponent implements OnInit {
       from.show();
     });
   }
+
+  public isAfterHours(): boolean {
+    let date = new Date();
+    let DayOfTheWeek = date.getDay();
+    let CurrentHour = date.getHours();
+
+    switch(DayOfTheWeek) {
+      // Sunday
+      case 0: { return true; }
+      // Monday
+      case 1: {
+        if(CurrentHour < 7 || CurrentHour >= 19) { return true; }
+        else { return false; }
+      }
+      // Tuesday
+      case 2: {
+        if(CurrentHour < 7 || CurrentHour >= 21) { return true; }
+        else { return false; }
+      }
+      // Wednesday
+      case 3: {
+        if(CurrentHour < 7 || CurrentHour >= 21) { return true; }
+        else { return false; }
+      }
+      // Thursday
+      case 4: {
+        if(CurrentHour < 7 || CurrentHour >= 21) { return true; }
+        else { return false; }
+      }
+      // Friday
+      case 5: {
+        if(CurrentHour < 7 || CurrentHour >= 20) { return true; }
+        else { return false; }
+      }
+      // Saturday
+      case 6: {
+        if(CurrentHour < 8 || CurrentHour >= 12) { return true; }
+        else { return false; }
+      }
+      default: { return false; }
+    }
+  }
 }
