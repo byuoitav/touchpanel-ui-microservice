d1c4dbfd5047ced43fdcf39aca9ffd625bfab30b
Joseph Blodgett <joseph.blodgett@gmail.com>
2018-12-21 08:43:44 -0700

    fixing query string

--- uiconfig/handlers.go
+++ uiconfig/handlers.go
@@ -61,7 +61,7 @@ func GetUIPath(context echo.Context) error {
 		if strings.EqualFold(hostname, panel.Hostname) {
 			//we also want to generate a query parameter to add to the end each time it refreshes.
 			str := GenRandString(4)
-			queryString := "?" + url.QueryEscape(fmt.Sprintf("%s%s", version, str))
+			queryString := fmt.Sprintf("?%s=%s", url.QueryEscape(version), url.QueryEscape(str))
 
 			return context.JSON(http.StatusOK, &APIHost{Hostname: panel.UIPath + queryString})
 		}
