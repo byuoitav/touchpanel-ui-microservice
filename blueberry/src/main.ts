b3260e44102bac077393b29b8f9782472f64193a
SAM <samuel.mera.ricardo@gmail.com>
2024-03-07 15:41:59 -0700

    fixing logging

--- blueberry/src/main.ts
+++ blueberry/src/main.ts
@@ -5,13 +5,13 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
-(<any>Window).log = {
+(window as any).log = {
   enable: () => {
     // create iframe, steal its console
     const i = Document.prototype.createElement.call(document, "iframe");
     i.style.display = "none";
     document.body.appendChild(i);
-    (<any>Window).console = i.contentWindow.console;
+    (window as any).console = i.contentWindow.console;
     
     console.log("Logging enabled.");
   },
@@ -25,6 +25,28 @@ import { environment } from "./environments/environment";
     window.console.info = function() {};
   }
 };
+
+/*
+(<any>window).log = {
+  enable: () => {
+    // create iframe, steal its console
+    const i = Document.prototype.createElement.call(document, "iframe");
+    i.style.display = "none";
+    document.body.appendChild(i);
+    (<any>window).console = i.contentWindow.console;
+    
+    console.log("Logging enabled.");
+  },
+  disable: () => {
+    console.log(
+      "Logging is disabled. To enable, call log.enable(), or reload this page with the query parameter of 'log' set to true."
+    );
+
+    window.console.debug = function() {};
+    window.console.log = function() {};
+    window.console.info = function() {};
+  }
+};*/
 
 if (environment.production) {
   enableProdMode();
--- cherry/src/main.ts
+++ cherry/src/main.ts
@@ -4,13 +4,34 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
-(<any>Window).log = {
+window["log"] = {
   enable: () => {
     // create iframe, steal its console
     const i = Document.prototype.createElement.call(document, "iframe");
     i.style.display = "none";
     document.body.appendChild(i);
-    (<any>Window).console = i.contentWindow.console;
+    window.console = i.contentWindow.console;
+    
+    console.log("Logging enabled.");
+  },
+  disable: () => {
+    console.log(
+      "Logging is disabled. To enable, call log.enable(), or reload this page with the query parameter of 'log' set to true."
+    );
+
+    window.console.debug = function() {};
+    window.console.log = function() {};
+    window.console.info = function() {};
+  }
+};
+
+/*(<any>window).log = {
+  enable: () => {
+    // create iframe, steal its console
+    const i = Document.prototype.createElement.call(document, "iframe");
+    i.style.display = "none";
+    document.body.appendChild(i);
+    (<any>window).console = i.contentWindow.console;
 
     console.log("Logging enabled.");
   },
@@ -19,11 +40,11 @@ import { environment } from "./environments/environment";
       "Logging is disabled. To enable, call log.enable(), or reload this page with the query parameter of 'log' set to true."
     );
 
+    window.console.debug = function() {};
     window.console.log = function() {};
     window.console.info = function() {};
-    window.console.debug = function() {};
   }
-};
+};*/
 
 if (environment.production) {
   enableProdMode();
